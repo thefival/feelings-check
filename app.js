5d388301af41e13c72aef5b9ca6fd58a8b8ade78
Ethan Dodgen <thefivalco@gmail.com>
2022-12-24 10:14:32 -0600

    housekeeping

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 
-const app = Express()
+const app = express()
+
+app.use('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.use('/user_input', (req, res) => {
+    console.log(req)
+})
 
 app.listen(3000)
 
